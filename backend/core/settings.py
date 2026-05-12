2330fec7422982cd132e154caa5b34e682580b8d
Rounak <rounak@pirlanta.in>
2026-05-12 10:09:48 +0530

    Applied UI Changes.

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-p)56=220*m5g@l+jrd=)nx#xp@gv_bbids7u@2_n-*%%tp45)w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
